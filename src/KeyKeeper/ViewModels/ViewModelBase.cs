94db853340be9bef4f0ef0ceb27ab68c1a380568
Slavasil <subvia@proton.me>
2025-10-29 15:53:28 +0300

    create Avalonia project from template
    + create directory structure
    + create solution
    + create .gitignore with some rules

---
+++ src/KeyKeeper/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace KeyKeeper.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ src/KeyKeeper/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace KeyKeeper.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
