e37d835b3a5df16be7afc2e0183f1b9838aeccda
Chernykh Aleksandr <dernet4@gmail.com>
2025-11-07 16:02:40 +0300

    Update MainWindowViewModel.cs

--- src/KeyKeeper/ViewModels/MainWindowViewModel.cs
+++ src/KeyKeeper/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace KeyKeeper.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using KeyKeeper.Views;
+
+namespace KeyKeeper.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    [RelayCommand]
+    private void OpenSettings()
+    {
+        var settingsWindow = new SettingsWindow();
+        settingsWindow.Show();
+    }
 }
