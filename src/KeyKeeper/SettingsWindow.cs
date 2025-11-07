52c0474a209420fd0928f701fd2ac293ae2e54aa
Chernykh Aleksandr <dernet4@gmail.com>
2025-11-07 16:01:38 +0300

    Update SettingsWindow.cs

--- src/KeyKeeper/SettingsWindow.cs
+++ src/KeyKeeper/SettingsWindow.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
 
-namespace KeyKeeper
+namespace KeyKeeper.Views;
+public class SettingsWindow : Window
 {
-    internal class SettingsWindow
+    public SettingsWindow()
     {
+        this.Title = "Настройки";
+        this.Width = 400;
+        this.Height = 300;
+        var textBlock = new TextBlock
+        {
+            Text = "Окно настроек",
+            HorizontalAlignment = Avalonia.Layout.HorizontalAlignment.Center,
+            VerticalAlignment = Avalonia.Layout.VerticalAlignment.Center,
+            FontSize = 16
+        };
+        this.Content = textBlock;
     }
 }
