355f7d562dda9bb5a042abc9ed8394411b049066
Slavasil <subvia@proton.me>
2025-11-23 18:13:31 +0300

    add class for constants related to the file format

---
+++ src/KeyKeeper/PasswordStore/FileFormatConstants.cs
@@ -0,0 +1,11 @@
+namespace KeyKeeper.PasswordStore;
+
+static class FileFormatConstants
+{
+    public const int MIN_MASTER_SALT_LEN = 8;
+    public const int MAX_MASTER_SALT_LEN = 40;
+    public const int MIN_AESKDF_ROUNDS = 10;
+    public const int MAX_AESKDF_ROUNDS = 65536;
+    public const byte ENCRYPT_ALGO_AES = 14;
+    public const byte KDF_TYPE_AESKDF = 195;
+}
