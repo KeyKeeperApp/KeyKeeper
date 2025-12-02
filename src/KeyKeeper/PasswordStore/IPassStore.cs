4fbebbbf80d385fae728c95a66a83cdb2ba3116d
Slavasil <subvia@proton.me>
2025-12-02 16:47:44 +0300

    add Lock and Unlock methods to IPassStore

--- src/KeyKeeper/PasswordStore/IPassStore.cs
+++ src/KeyKeeper/PasswordStore/IPassStore.cs
@@ -1,7 +1,13 @@
+using KeyKeeper.PasswordStore.Crypto;
+
 namespace KeyKeeper.PasswordStore;
 
 public interface IPassStore
 {
+    bool Locked { get; }
+
     IPassStoreDirectory GetRootDirectory();
     int GetTotalEntryCount();
+    void Unlock(CompositeKey key);
+    void Lock();
 }
