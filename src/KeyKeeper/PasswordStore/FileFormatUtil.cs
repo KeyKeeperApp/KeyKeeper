5c8a63f56af2aafc443256ab7a947d17db13b335
Slavasil <subvia@proton.me>
2025-12-04 02:09:37 +0300

    implement some missing methods in FileFormatUtil

--- src/KeyKeeper/PasswordStore/FileFormatUtil.cs
+++ src/KeyKeeper/PasswordStore/FileFormatUtil.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Buffers.Binary;
 using System.IO;
 using System.Text;
 
@@ -22,6 +23,28 @@ static class FileFormatUtil
         return size;
     }
 
+    public static ulong ReadVarUint16(Stream str)
+    {
+        ulong number = 0;
+        int i = 0;
+        while (true)
+        {
+            int b = str.ReadByte();
+            if (b == -1)
+                throw PassStoreFileException.UnexpectedEndOfFile;
+            number |= (ulong)b << i;
+            i += 8;
+            b = str.ReadByte();
+            if (b == -1)
+                throw PassStoreFileException.UnexpectedEndOfFile;
+            number |= (ulong)(b & 0x7f) << i;
+            if ((b & 0x80) == 0)
+                break;
+            i += 7;
+        }
+        return number;
+    }
+
     public static int WriteU8TaggedString(Stream str, string s)
     {
         byte[] b = Encoding.UTF8.GetBytes(s);
@@ -41,5 +64,17 @@ static class FileFormatUtil
         str.WriteByte((byte)(b.Length >> 8));
         str.Write(b);
         return b.Length + 2;
+    }
+
+    public static string ReadU16TaggedString(Stream str)
+    {
+        byte[] lenBytes = new byte[2];
+        if (str.Read(lenBytes) < 2)
+            throw PassStoreFileException.UnexpectedEndOfFile;
+        int len = BinaryPrimitives.ReadUInt16LittleEndian(lenBytes);
+        byte[] s = new byte[len];
+        if (str.Read(s) < len)
+            throw PassStoreFileException.UnexpectedEndOfFile;
+        return Encoding.UTF8.GetString(s);
     }
 }
