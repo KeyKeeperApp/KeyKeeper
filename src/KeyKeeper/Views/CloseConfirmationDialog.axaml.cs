63194d37d6e571232450999015c7a68a9f3ead31
InspectorIT <sshipunovv008@gmail.com>
2026-03-01 23:50:18 +0300

    add CloseConfirmationDialog

--- src/KeyKeeper/Views/CloseConfirmationDialog.axaml.cs
+++ src/KeyKeeper/Views/CloseConfirmationDialog.axaml.cs
@@ -1,12 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Avalonia.Controls;
+using Avalonia.Interactivity;
 
-namespace KeyKeeper.Views
+namespace KeyKeeper.Views;
+
+public enum CloseConfirmationResult
 {
-    internal class CloseConfirmationDialog
+    Save,
+    Discard,
+    Cancel,
+}
+
+public partial class CloseConfirmationDialog : Window
+{
+    private bool closingWithResult;
+
+    public CloseConfirmationDialog()
     {
+        InitializeComponent();
+    }
+
+    protected override void OnClosing(WindowClosingEventArgs e)
+    {
+        if (!closingWithResult)
+        {
+            e.Cancel = true;
+            closingWithResult = true;
+            Close(CloseConfirmationResult.Cancel);
+            return;
+        }
+
+        base.OnClosing(e);
+    }
+
+    private void Save_Click(object? sender, RoutedEventArgs e)
+    {
+        closingWithResult = true;
+        Close(CloseConfirmationResult.Save);
+    }
+
+    private void Discard_Click(object? sender, RoutedEventArgs e)
+    {
+        closingWithResult = true;
+        Close(CloseConfirmationResult.Discard);
+    }
+
+    private void Cancel_Click(object? sender, RoutedEventArgs e)
+    {
+        closingWithResult = true;
+        Close(CloseConfirmationResult.Cancel);
     }
 }
